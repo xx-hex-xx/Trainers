18ab51b14ebaf4f88b1bff02d4f1afd15f238a7c
hex <7x.hex.x7@gmail.com>
2025-03-02 14:35:53 +0300

    Update ExeMD5Hashes.h

--- Data/Public/AC4BF/ExeMD5Hashes.h
+++ Data/Public/AC4BF/ExeMD5Hashes.h
@@ -1,1 +1,25 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+const QString strExeMD5Hash_Steam = "2058342866688f780c8b34526a65bc35"; // 1.08
+const QString strExeMD5Hash_EpicGamesStore = "";
+const QString strExeMD5Hash_UbisoftConnect = "2058342866688f780c8b34526a65bc35"; // 1.08
+const QString strExeMD5Hash_EAApp = "";
+const QString strExeMD5Hash_GOGGALAXY = "";
+const QString strExeMD5Hash_BattleNet = "";
+const QString strExeMD5Hash_MicrosoftStore = "";
+const QString strExeMD5Hash_VKPlay = "";
+const QString strExeMD5Hash_RoclstarGamesLauncher = "";
+const QString strExeMD5Hash_Pirated_1 = "6fbea697e557f388d243eb1212328782"; // Repack by xatab - 1.07
+const QString strExeMD5Hash_Pirated_2 = "2058342866688f780c8b34526a65bc35"; // Repack by dixen18 - 1.07
+const QString strExeMD5Hash_Pirated_3 = "n/d"; // Repack by =nemos= - 
+const QString strExeMD5Hash_Pirated_4 = "";
+const QString strExeMD5Hash_Pirated_5 = "";
+const QString strExeMD5Hash_Pirated_6 = "";
+const QString strExeMD5Hash_Pirated_7 = "";
+const QString strExeMD5Hash_Pirated_8 = "";
+const QString strExeMD5Hash_Pirated_9 = "";
+const QString strExeMD5Hash_Pirated_10 = "";
 
+// ====[ OTHERS ]================================================================
+[strExeMD5Hash_01]b066647dff1ae33d459572b94102231d[strExeMD5Hash] // old license 1.07 (02.2018)
+[strExeMD5Hash_02]6fbea697e557f388d243eb1212328782[strExeMD5Hash] // old license 1.07 (03.2014)
