1ddfd449ab1bb96bbeda1fa97dcc184c97498ea8
hex <7x.hex.x7@gmail.com>
2025-04-29 12:10:36 +0300

    Update ExeMD5Hashes.h

--- Data/Public/AC4BF/1.8/Others/ExeMD5Hashes.h
+++ Data/Public/AC4BF/1.8/Others/ExeMD5Hashes.h
@@ -1,25 +1,13 @@
 // |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
 // ====[ MAIN ]==================================================================
-const QString strExeMD5Hash_Steam = "2058342866688f780c8b34526a65bc35"; // 1.08
-const QString strExeMD5Hash_EpicGamesStore = "";
-const QString strExeMD5Hash_UbisoftConnect = "2058342866688f780c8b34526a65bc35"; // 1.08
-const QString strExeMD5Hash_EAApp = "";
-const QString strExeMD5Hash_GOGGALAXY = "";
-const QString strExeMD5Hash_BattleNet = "";
-const QString strExeMD5Hash_MicrosoftStore = "";
-const QString strExeMD5Hash_VKPlay = "";
-const QString strExeMD5Hash_RoclstarGamesLauncher = "";
-const QString strExeMD5Hash_SteamUplay2018 = "b066647dff1ae33d459572b94102231d"; // 1.07
-const QString strExeMD5Hash_Pirated_1 = "6fbea697e557f388d243eb1212328782"; // Repack by xatab - 1.07
-const QString strExeMD5Hash_Pirated_2 = "2058342866688f780c8b34526a65bc35"; // Repack by dixen18 - 1.08
-const QString strExeMD5Hash_Pirated_3 = "2058342866688f780c8b34526a65bc35"; // Repack by Decepticon - 1.08
-const QString strExeMD5Hash_Pirated_4 = "2058342866688f780c8b34526a65bc35"; // Repack by Wanterlude - 1.08
-const QString strExeMD5Hash_Pirated_5 = "8dbd0fc0408da34fc3a9c9c253b432ae"; // Repack by DODI - 1.07
-const QString strExeMD5Hash_Pirated_6 = "";
-const QString strExeMD5Hash_Pirated_7 = "";
-const QString strExeMD5Hash_Pirated_8 = "";
-const QString strExeMD5Hash_Pirated_9 = "";
-const QString strExeMD5Hash_Pirated_10 = "";
+strExeMD5Hash_Steam = "2058342866688f780c8b34526a65bc35"; // 1.08
+strExeMD5Hash_UbisoftConnect = "2058342866688f780c8b34526a65bc35"; // 1.08
+strExeMD5Hash_SteamUplay2018 = "b066647dff1ae33d459572b94102231d"; // 1.07
+strExeMD5Hash_Other_1 = "6fbea697e557f388d243eb1212328782"; // 1.07
+strExeMD5Hash_Other_2 = "2058342866688f780c8b34526a65bc35"; // 1.08
+strExeMD5Hash_Other_3 = "2058342866688f780c8b34526a65bc35"; // 1.08
+strExeMD5Hash_Other_4 = "2058342866688f780c8b34526a65bc35"; // 1.08
+strExeMD5Hash_Other_5 = "8dbd0fc0408da34fc3a9c9c253b432ae"; // 1.07
 
 // ====[ ADDITIONALS ]===========================================================
 [strExeMD5Hash_1]352b341e5292d4be6e42419a00b9e707[strExeMD5Hash] // Steam & Uplay 1.08, window mode (07.2023)
