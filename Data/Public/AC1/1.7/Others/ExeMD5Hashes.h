e235c1013abecfeee627aa60acb1eb8c6a5abf83
hex <7x.hex.x7@gmail.com>
2025-04-29 10:48:04 +0300

    Update ExeMD5Hashes.h

--- Data/Public/AC1/1.7/Others/ExeMD5Hashes.h
+++ Data/Public/AC1/1.7/Others/ExeMD5Hashes.h
@@ -1,30 +1,18 @@
 // |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
 // ====[ MAIN ]==================================================================
-const QString strExeMD5Hash_Steam_Dx9 = "8e72c3333743780e43bc2c34bbf625f9"; // 1.02
-const QString strExeMD5Hash_Steam_Dx10 = "ca87753255e2d14b1f18bb737c643792"; // 1.02
-const QString strExeMD5Hash_EpicGamesStore = "";
-const QString strExeMD5Hash_UbisoftConnect_Dx9 = "c85473cc4f2a5d1391a6d77bceaf276e"; // 1.02
-const QString strExeMD5Hash_UbisoftConnect_Dx10 = "86924b021ddefdecd96ebea48aa79701"; // 1.02
-const QString strExeMD5Hash_EAApp = "";
-const QString strExeMD5Hash_GOGGALAXY_Dx9 = "8e72c3333743780e43bc2c34bbf625f9"; // 1.02
-const QString strExeMD5Hash_GOGGALAXY_Dx10 = "ca87753255e2d14b1f18bb737c643792"; // 1.02
-const QString strExeMD5Hash_BattleNet = "";
-const QString strExeMD5Hash_MicrosoftStore = "";
-const QString strExeMD5Hash_VKPlay = "";
-const QString strExeMD5Hash_RoclstarGamesLauncher = "";
-const QString strExeMD5Hash_Pirated_1_Dx9 = "516fdff75e334c11785e5e4e945d04c9"; // Repack by N.A.R.E.K.96 - 1.02
-const QString strExeMD5Hash_Pirated_1_Dx10 = "f4e6794fc5c513a97a27eaa7addb7f0e"; // Repack by N.A.R.E.K.96 - 1.02
-const QString strExeMD5Hash_Pirated_2_Dx9 = "516fdff75e334c11785e5e4e945d04c9"; // Repack by R.G. Mechanics - 1.02
-const QString strExeMD5Hash_Pirated_2_Dx10 = "f4e6794fc5c513a97a27eaa7addb7f0e"; // Repack by R.G. Mechanics - 1.02
-const QString strExeMD5Hash_Pirated_3_Dx9 = "516fdff75e334c11785e5e4e945d04c9"; // Repack by R.G. NoLimits-Team Games - 1.02
-const QString strExeMD5Hash_Pirated_3_Dx10 = "f4e6794fc5c513a97a27eaa7addb7f0e"; // Repack by R.G. NoLimits-Team Games - 1.02
-const QString strExeMD5Hash_Pirated_4_Dx9 = "8e72c3333743780e43bc2c34bbf625f9"; // Repack by DODI - 1.02
-const QString strExeMD5Hash_Pirated_4_Dx10 = "ca87753255e2d14b1f18bb737c643792"; // Repack by DODI - 1.02
-const QString strExeMD5Hash_Pirated_5 = "";
-const QString strExeMD5Hash_Pirated_6 = "";
-const QString strExeMD5Hash_Pirated_7 = "";
-const QString strExeMD5Hash_Pirated_8 = "";
-const QString strExeMD5Hash_Pirated_9 = "";
-const QString strExeMD5Hash_Pirated_10 = "";
+strExeMD5Hash_Steam_Dx9 = "8e72c3333743780e43bc2c34bbf625f9"; // 1.02
+strExeMD5Hash_Steam_Dx10 = "ca87753255e2d14b1f18bb737c643792"; // 1.02
+strExeMD5Hash_UbisoftConnect_Dx9 = "c85473cc4f2a5d1391a6d77bceaf276e"; // 1.02
+strExeMD5Hash_UbisoftConnect_Dx10 = "86924b021ddefdecd96ebea48aa79701"; // 1.02
+strExeMD5Hash_GOGGALAXY_Dx9 = "8e72c3333743780e43bc2c34bbf625f9"; // 1.02
+strExeMD5Hash_GOGGALAXY_Dx10 = "ca87753255e2d14b1f18bb737c643792"; // 1.02
+strExeMD5Hash_Other_1_Dx9 = "516fdff75e334c11785e5e4e945d04c9"; // 1.02
+strExeMD5Hash_Other_1_Dx10 = "f4e6794fc5c513a97a27eaa7addb7f0e"; // 1.02
+strExeMD5Hash_Other_2_Dx9 = "516fdff75e334c11785e5e4e945d04c9"; // 1.02
+strExeMD5Hash_Other_2_Dx10 = "f4e6794fc5c513a97a27eaa7addb7f0e"; // 1.02
+strExeMD5Hash_Other_3_Dx9 = "516fdff75e334c11785e5e4e945d04c9"; // 1.02
+strExeMD5Hash_Other_3_Dx10 = "f4e6794fc5c513a97a27eaa7addb7f0e"; // 1.02
+strExeMD5Hash_Other_4_Dx9 = "8e72c3333743780e43bc2c34bbf625f9"; // 1.02
+strExeMD5Hash_Other_4_Dx10 = "ca87753255e2d14b1f18bb737c643792"; // 1.02
 
 // ====[ ADDITIONALS ]===========================================================
