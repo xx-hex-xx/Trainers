631dc1ec7506410fe9aa4251c1bb07d85dab3462
hex <7x.hex.x7@gmail.com>
2025-04-29 12:00:55 +0300

    Update ExeMD5Hashes.h

--- Data/Public/ACR/1.7/Others/ExeMD5Hashes.h
+++ Data/Public/ACR/1.7/Others/ExeMD5Hashes.h
@@ -1,23 +1,10 @@
 // |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
 // ====[ MAIN ]==================================================================
-const QString strExeMD5Hash_Steam = "11330debf358963b33cc2d79e1b3d288"; // 1.03
-const QString strExeMD5Hash_EpicGamesStore = "";
-const QString strExeMD5Hash_UbisoftConnect = "11330debf358963b33cc2d79e1b3d288"; // 1.03
-const QString strExeMD5Hash_EAApp = "";
-const QString strExeMD5Hash_GOGGALAXY = "";
-const QString strExeMD5Hash_BattleNet = "";
-const QString strExeMD5Hash_MicrosoftStore = "";
-const QString strExeMD5Hash_VKPlay = "";
-const QString strExeMD5Hash_RoclstarGamesLauncher = "";
-const QString strExeMD5Hash_Pirated_1 = "8d39af08012f73e72affd61cba96d5a0"; // Repack by xatab - 1.03
-const QString strExeMD5Hash_Pirated_2 = "8d39af08012f73e72affd61cba96d5a0"; // Repack by Fenixx - 1.03
-const QString strExeMD5Hash_Pirated_3 = "9c5e5533f26b1a705bdddac985753680"; // Repack by dixen18 - 1.03
-const QString strExeMD5Hash_Pirated_4 = "7ecf9c5ee74920bad69b024aba697377"; // Repack by DODI - 1.03
-const QString strExeMD5Hash_Pirated_5 = "";
-const QString strExeMD5Hash_Pirated_6 = "";
-const QString strExeMD5Hash_Pirated_7 = "";
-const QString strExeMD5Hash_Pirated_8 = "";
-const QString strExeMD5Hash_Pirated_9 = "";
-const QString strExeMD5Hash_Pirated_10 = "";
+strExeMD5Hash_Steam = "11330debf358963b33cc2d79e1b3d288"; // 1.03
+strExeMD5Hash_UbisoftConnect = "11330debf358963b33cc2d79e1b3d288"; // 1.03
+strExeMD5Hash_Other_1 = "8d39af08012f73e72affd61cba96d5a0"; // 1.03
+strExeMD5Hash_Other_2 = "8d39af08012f73e72affd61cba96d5a0"; // 1.03
+strExeMD5Hash_Other_3 = "9c5e5533f26b1a705bdddac985753680"; // 1.03
+strExeMD5Hash_Other_4 = "7ecf9c5ee74920bad69b024aba697377"; // 1.03
 
 // ====[ ADDITIONALS ]===========================================================
