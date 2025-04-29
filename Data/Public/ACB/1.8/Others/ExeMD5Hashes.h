6b69b2cfe55b4cf712d44c80fecdf9eb234124c5
hex <7x.hex.x7@gmail.com>
2025-04-29 11:56:20 +0300

    Update ExeMD5Hashes.h

--- Data/Public/ACB/1.8/Others/ExeMD5Hashes.h
+++ Data/Public/ACB/1.8/Others/ExeMD5Hashes.h
@@ -1,23 +1,11 @@
 // |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
 // ====[ MAIN ]==================================================================
-const QString strExeMD5Hash_Steam = "82039fb1b10734e770803274a628592a"; // 1.03
-const QString strExeMD5Hash_EpicGamesStore = "";
-const QString strExeMD5Hash_UbisoftConnect = "2dfdd878ee410922341fd40de81829bb"; // 1.03
-const QString strExeMD5Hash_EAApp = "";
-const QString strExeMD5Hash_GOGGALAXY = "";
-const QString strExeMD5Hash_BattleNet = "";
-const QString strExeMD5Hash_MicrosoftStore = "";
-const QString strExeMD5Hash_VKPlay = "";
-const QString strExeMD5Hash_RoclstarGamesLauncher = "";
-const QString strExeMD5Hash_Pirated_1 = "6882e5fad03f4ab040ff1840f2a1cb61"; // Crack by SKiDROW - 1.03
-const QString strExeMD5Hash_Pirated_2 = "7ca53bae82a4a19e2a0a46b1ddf3f355"; // Repack by xatab - 1.03
-const QString strExeMD5Hash_Pirated_3 = "7ca53bae82a4a19e2a0a46b1ddf3f355"; // Repack by R. G. Catalyst - 1.03
-const QString strExeMD5Hash_Pirated_4 = "f8a3ccd99439a3956022157b66c68fb9"; // Repack by dixen18 - 1.03
-const QString strExeMD5Hash_Pirated_5 = "23221f71d7416883dcd71c802f7fc7fe"; // Repack by DODI - 1.03
-const QString strExeMD5Hash_Pirated_6 = "";
-const QString strExeMD5Hash_Pirated_7 = "";
-const QString strExeMD5Hash_Pirated_8 = "";
-const QString strExeMD5Hash_Pirated_9 = "";
-const QString strExeMD5Hash_Pirated_10 = "";
+strExeMD5Hash_Steam = "82039fb1b10734e770803274a628592a"; // 1.03
+strExeMD5Hash_UbisoftConnect = "2dfdd878ee410922341fd40de81829bb"; // 1.03
+strExeMD5Hash_Other_1 = "6882e5fad03f4ab040ff1840f2a1cb61"; // 1.03
+strExeMD5Hash_Other_2 = "7ca53bae82a4a19e2a0a46b1ddf3f355"; // 1.03
+strExeMD5Hash_Other_3 = "7ca53bae82a4a19e2a0a46b1ddf3f355"; // 1.03
+strExeMD5Hash_Other_4 = "f8a3ccd99439a3956022157b66c68fb9"; // 1.03
+strExeMD5Hash_Other_5 = "23221f71d7416883dcd71c802f7fc7fe"; // 1.03
 
 // ====[ ADDITIONALS ]===========================================================
