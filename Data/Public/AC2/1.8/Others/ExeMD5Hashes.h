2501e91d4ec71938084e03bde693bd403a214e15
hex <7x.hex.x7@gmail.com>
2025-04-29 10:58:41 +0300

    Update ExeMD5Hashes.h

--- Data/Public/AC2/1.8/Others/ExeMD5Hashes.h
+++ Data/Public/AC2/1.8/Others/ExeMD5Hashes.h
@@ -1,24 +1,10 @@
 // |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
 // ====[ MAIN ]==================================================================
-const QString strExeMD5Hash_Steam = "7d6b866eeacfc7a69a00f800a529bf8e"; // 1.01
-const QString strExeMD5Hash_EpicGamesStore = "";
-const QString strExeMD5Hash_UbisoftConnect = "c69aff7f22ddedaee6e2ba72afc35554"; // 1.01
-const QString strExeMD5Hash_UbisoftConnect_Retail_RU = "14d0a338110bc52afb2db9818d502ac5"; // 1.01
-const QString strExeMD5Hash_EAApp = "";
-const QString strExeMD5Hash_GOGGALAXY = "";
-const QString strExeMD5Hash_BattleNet = "";
-const QString strExeMD5Hash_MicrosoftStore = "";
-const QString strExeMD5Hash_VKPlay = "";
-const QString strExeMD5Hash_RoclstarGamesLauncher = "";
-const QString strExeMD5Hash_Pirated_1 = "d3950de8edd198baccf87671c12ab88d"; // Repack by xatab - 1.01
-const QString strExeMD5Hash_Pirated_2 = "77fced872cd0434ba4ea49df584f4538"; // Repack by VITEK - 1.01
-const QString strExeMD5Hash_Pirated_3 = "7753f04ad6fb8379b09014ce727a4778"; // Repack by DODI - 1.01
-const QString strExeMD5Hash_Pirated_4 = "";
-const QString strExeMD5Hash_Pirated_5 = "";
-const QString strExeMD5Hash_Pirated_6 = "";
-const QString strExeMD5Hash_Pirated_7 = "";
-const QString strExeMD5Hash_Pirated_8 = "";
-const QString strExeMD5Hash_Pirated_9 = "";
-const QString strExeMD5Hash_Pirated_10 = "";
+strExeMD5Hash_Steam = "7d6b866eeacfc7a69a00f800a529bf8e"; // 1.01
+strExeMD5Hash_UbisoftConnect = "c69aff7f22ddedaee6e2ba72afc35554"; // 1.01
+strExeMD5Hash_UbisoftConnect_Retail_RU = "14d0a338110bc52afb2db9818d502ac5"; // 1.01
+strExeMD5Hash_Other_1 = "d3950de8edd198baccf87671c12ab88d"; // 1.01
+strExeMD5Hash_Other_2 = "77fced872cd0434ba4ea49df584f4538"; // 1.01
+strExeMD5Hash_Other_3 = "7753f04ad6fb8379b09014ce727a4778"; // 1.01
 
 // ====[ ADDITIONALS ]===========================================================
