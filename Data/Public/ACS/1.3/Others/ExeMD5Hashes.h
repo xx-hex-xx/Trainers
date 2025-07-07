8d4aa93880e66db10a8e5c4abf70ff79dc850157
hex <7x.hex.x7@gmail.com>
2025-07-07 22:00:33 +0300

    Create ExeMD5Hashes.h

---
+++ Data/Public/ACS/1.3/Others/ExeMD5Hashes.h
@@ -0,0 +1,10 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+strExeMD5Hash_Steam = "930794066e296b84a2984729efcaa026"; // 1.51 - Gold Edition
+strExeMD5Hash_EpicGamesStore = "6a046ab7ce6363117779bd10a868795b"; // 1.51 - Standart Edition
+strExeMD5Hash_UbisoftConnect = "930794066e296b84a2984729efcaa026"; // 1.51 - Gold Edition
+strExeMD5Hash_Other_1 = "930794066e296b84a2984729efcaa026"; // 1.51 (Gold Edition)
+strExeMD5Hash_Other_2 = "930794066e296b84a2984729efcaa026"; // 1.51 (Gold Edition)
+strExeMD5Hash_Other_3 = "930794066e296b84a2984729efcaa026"; // 1.51 (Gold Edition)
+
+// ====[ ADDITIONALS ]===========================================================
