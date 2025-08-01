df27846ac39411b377c9c32b13aae56a3d0e050e
hex <7x.hex.x7@gmail.com>
2025-08-01 19:51:52 +0300

    Create ExeMD5Hashes.h

---
+++ Data/Public/M2033R/1.3/Others/ExeMD5Hashes.h
@@ -0,0 +1,12 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+strExeMD5Hash_Steam = "298c5e5ae6d54d236d580c25a0d3f685"; // 1.0.0.3
+strExeMD5Hash_EpicGamesStore = "851cf1f776e37a9ba2b62b374bfde291"; // 1.0.1.10
+strExeMD5Hash_GOGGALAXY = "0d8f599016f75de3c54047af3866bbe4"; // 1.0.0.3
+strExeMD5Hash_Other_1 = "84a46c53a4a15b72e16d8cad7075d335"; // 1.0.0.3
+strExeMD5Hash_Other_2 = "84a46c53a4a15b72e16d8cad7075d335"; // 1.0.0.3
+strExeMD5Hash_Other_3 = "298c5e5ae6d54d236d580c25a0d3f685"; // 1.0.0.3
+strExeMD5Hash_Other_4 = "7cce19c239f2db9c2ffbcd5bb69c28fa"; // 1.0.0.1
+strExeMD5Hash_Other_5 = "d88db14bafbfe3ffc3cf73d2681bba24"; // 1.0.0.3
+
+// ====[ ADDITIONALS ]===========================================================
