7b2b9228c182401625659a212d34e7cffd1cb9ed
hex <7x.hex.x7@gmail.com>
2025-08-03 16:55:02 +0300

    Create ExeMD5Hashes.h

---
+++ Data/Public/MLLR/1.3/Others/ExeMD5Hashes.h
@@ -0,0 +1,9 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+strExeMD5Hash_Steam = "9380946ecca2ceebfbc4bc2bc24306c6"; // 1.0.0.3
+strExeMD5Hash_EpicGamesStore = "851cf1f776e37a9ba2b62b374bfde291"; // 1.0.1.10
+strExeMD5Hash_GOGGALAXY = "0d8f599016f75de3c54047af3866bbe4"; // 1.0.0.3
+strExeMD5Hash_Other_1 = "84a46c53a4a15b72e16d8cad7075d335"; // 1.0.0.3
+strExeMD5Hash_Other_2 = "84a46c53a4a15b72e16d8cad7075d335"; // 1.0.0.3
+
+// ====[ ADDITIONALS ]===========================================================
