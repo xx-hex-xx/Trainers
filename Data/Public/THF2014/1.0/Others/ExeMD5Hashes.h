91f4237ed336acc448732538d351de05fa9b4178
hex <7x.hex.x7@gmail.com>
2025-12-06 11:03:35 +0300

    Create ExeMD5Hashes.h

---
+++ Data/Public/THF2014/1.0/Others/ExeMD5Hashes.h
@@ -0,0 +1,10 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+strExeMD5Hash_Steam = "d2faa6fd6c51f833bbac61cd6d63d106"; // 1.7
+strExeMD5Hash_GOGGALAXY = "f69d9dc0d089108bbf095032b45e4c5f"; // 1.7
+strExeMD5Hash_Other_1 = "d2faa6fd6c51f833bbac61cd6d63d106"; // 1.7
+strExeMD5Hash_Other_2 = "f247a5f767a11aec98238d116a5e3185"; // 1.7
+strExeMD5Hash_Other_3 = "f69d9dc0d089108bbf095032b45e4c5f"; // 1.7
+strExeMD5Hash_Other_3 = "d2faa6fd6c51f833bbac61cd6d63d106"; // 1.7
+
+// ====[ ADDITIONALS ]===========================================================
