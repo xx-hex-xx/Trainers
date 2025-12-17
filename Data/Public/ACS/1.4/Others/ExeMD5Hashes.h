940046d572e3b91962907191ad87a21839b1f891
hex <7x.hex.x7@gmail.com>
2025-12-17 23:01:43 +0300

    Create ExeMD5Hashes.h

---
+++ Data/Public/ACS/1.4/Others/ExeMD5Hashes.h
@@ -0,0 +1,13 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+strExeMD5Hash_Steam_1_51 = "930794066e296b84a2984729efcaa026"; // 1.51 - Gold Edition
+strExeMD5Hash_Steam_1_53 = "6db932b206da493b50c16f625a9307d8"; // 1.53 - Gold Edition
+strExeMD5Hash_EpicGamesStore_1_51 = "6a046ab7ce6363117779bd10a868795b"; // 1.51 - Standart Edition
+strExeMD5Hash_EpicGamesStore_1_53 = "6db932b206da493b50c16f625a9307d8"; // 1.53
+strExeMD5Hash_UbisoftConnect_1_51 = "930794066e296b84a2984729efcaa026"; // 1.51 - Gold Edition
+strExeMD5Hash_UbisoftConnect_1_53 = "6db932b206da493b50c16f625a9307d8"; // 1.53
+strExeMD5Hash_Other_1 = "930794066e296b84a2984729efcaa026"; // 1.51 (Gold Edition)
+strExeMD5Hash_Other_2 = "930794066e296b84a2984729efcaa026"; // 1.51 (Gold Edition)
+strExeMD5Hash_Other_3 = "930794066e296b84a2984729efcaa026"; // 1.51 (Gold Edition)
+
+// ====[ ADDITIONALS ]===========================================================
