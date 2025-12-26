69c1afec1da9687e5479d5e803aa92d30dbb2edf
hex <7x.hex.x7@gmail.com>
2025-12-26 12:42:05 +0300

    Create ExeMD5Hashes.h

---
+++ Data/Public/ACOR/1.0/Others/ExeMD5Hashes.h
@@ -0,0 +1,6 @@
+const QString strExeMD5Hash_Steam = "8ad50d74763169ae03cfa6a29416f4bd"; // 1.62
+const QString strExeMD5Hash_UbisoftConnect = "27ea5af74195032b48520e7b165ec68d"; // 1.62
+const QString strExeMD5Hash_Other_1 = "de1be45f4b84ac4623aa1911e44e385c"; // 1.51
+const QString strExeMD5Hash_Other_2 = "06b9ea8ef50a4b8f7759f2aa61a89f5a"; // 1.51
+const QString strExeMD5Hash_Other_3 = "54c2d31c5a24c66dd628b1844990039c"; // 1.52
+const QString strExeMD5Hash_Other_4 = "18a72d8de8549c5053e1d041a791b00d"; // 1.52
