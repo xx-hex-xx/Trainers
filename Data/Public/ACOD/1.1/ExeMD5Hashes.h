193c04e2502b97238a0b0ae01c01a4e860c1217c
hex <7x.hex.x7@gmail.com>
2026-02-15 18:20:57 +0300

    Delete Data/Public/ACOD/1.1/ExeMD5Hashes.h

--- Data/Public/ACOD/1.1/ExeMD5Hashes.h
+++
@@ -1,9 +0,0 @@
-// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
-// ====[ MAIN ]==================================================================
-const QString strExeMD5Hash_Steam = "55d3eda99cc5563d30423431b89fff4a"; // 1.5.6
-const QString strExeMD5Hash_UbisoftConnect_156 = "410624ecc125c282f895f2e7cba43b57"; // 1.5.6
-const QString strExeMD5Hash_UbisoftConnect_153 = "1deee38c6e98adc5bc0bdd246b2b26ce"; // 1.5.3
-const QString strExeMD5Hash_Other_1 = "ebb248a34bf0e34cd18c3415c74e28a1"; // 1.5.3
-const QString strExeMD5Hash_Other_2 = "ebb248a34bf0e34cd18c3415c74e28a1"; // 1.5.3
-
-// ====[ ADDITIONALS ]===========================================================
