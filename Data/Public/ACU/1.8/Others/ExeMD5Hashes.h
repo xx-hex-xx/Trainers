990848939df179350bf760ff84146b2ab7f74444
hex <7x.hex.x7@gmail.com>
2026-03-11 16:15:23 +0300

    Create ExeMD5Hashes.h

---
+++ Data/Public/ACU/1.8/Others/ExeMD5Hashes.h
@@ -0,0 +1,9 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+const QString strExeMD5Hash_Steam_150 = "42d4cd181411288244a237ace58f5796"; // 1.5.0
+const QString strExeMD5Hash_Steam_151 = "7146400b1bb1654c48f787af61befe12"; // 1.5.1
+const QString strExeMD5Hash_UbisoftConnect_150 = "42d4cd181411288244a237ace58f5796"; // 1.5.0
+const QString strExeMD5Hash_UbisoftConnect_151 = "7146400b1bb1654c48f787af61befe12"; // 1.5.1
+const QString strExeMD5Hash_Other_1 = "42d4cd181411288244a237ace58f5796"; // 1.5.0
+
+// ====[ ADDITIONALS ]===========================================================
