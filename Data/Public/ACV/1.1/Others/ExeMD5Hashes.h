0559263071a0f563c00db5e730591608f1aca55a
hex <7x.hex.x7@gmail.com>
2026-06-13 15:30:59 +0300

    Add ExeMD5Hashes.h with MD5 hashes for executables

---
+++ Data/Public/ACV/1.1/Others/ExeMD5Hashes.h
@@ -0,0 +1,13 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+const QString strExeMD5Hash_Steam = "f55a00a11d9812dccd0f403f817c0030"; // 1.7.0
+const QString strExeMD5Hash_Steam_Patched = "1091ff6598e33d25eb4b2ce2af0e85c7"; // 1.7.0
+const QString strExeMD5Hash_UbisoftConnect_1 = "66c9732c34842a644164186988f96a2b"; // 1.7.0
+const QString strExeMD5Hash_UbisoftConnect_2 = "5d4116dfb206cb13ac3f4a3e19bb05d7"; // 1.7.0
+const QString strExeMD5Hash_Other_1 = "d79c62ecb8d8b3897d081795ecb40876"; // 1.7.0
+const QString strExeMD5Hash_Other_2 = "f118ff9c43956bb1b28c8d898866c0b6"; // 1.7.0
+const QString strExeMD5Hash_Other_3 = "ad96281a8a29bf8199d99e7e739c5262"; // 1.7.0
+
+// ====[ ADDITIONALS ]===========================================================
+[strExeMD5Hash_1]f89f96b9af927b0f9cd79611b5a6f32f[strExeMD5Hash] // Ubisoft Connect - 1.7.0 (temp?)
+[strExeMD5Hash_2]6c6d5b4d0903d4b6e6895dee82dfe03e[strExeMD5Hash] // Ubisoft Connect - 1.7.0 (patched for AA)
