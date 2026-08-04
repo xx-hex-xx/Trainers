33c14aaa53804844b9febdb136a8f6a204b84b2a
hex <7x.hex.x7@gmail.com>
2026-08-04 09:15:47 +0300

    Add EXE MD5 hashes for various platforms

---
+++ Data/Public/ACM/1.0/Others/ExeMD5Hashes.h
@@ -0,0 +1,8 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+const QString strExeMD5Hash_Steam = "662432025e88aefabc53c6786a8c748f"; // 1.1.1
+const QString strExeMD5Hash_UbisoftConnect = "75a493343f6da1b0bf38ff8c039512cf"; // 1.1.1
+const QString strExeMD5Hash_UbisoftConnect_Plus = "bdfba65a0d75c6578effb032462ad118"; // 1.1.1
+const QString strExeMD5Hash_Other_1 = "d19b239853ab89b61eb30aea89110084"; // 1.1.1
+
+// ====[ ADDITIONALS ]===========================================================
