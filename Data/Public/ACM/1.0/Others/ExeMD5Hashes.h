f9ff7438f831d19f992ed325774ea0d4e84bd837
hex <7x.hex.x7@gmail.com>
2026-09-22 22:15:03 +0300

    Add new MD5 hash for additional executable

--- Data/Public/ACM/1.0/Others/ExeMD5Hashes.h
+++ Data/Public/ACM/1.0/Others/ExeMD5Hashes.h
@@ -6,3 +6,4 @@ const QString strExeMD5Hash_UbisoftConnect_Plus = "bdfba65a0d75c6578effb032462ad
 const QString strExeMD5Hash_Other_1 = "d19b239853ab89b61eb30aea89110084"; // 1.1.1
 
 // ====[ ADDITIONALS ]===========================================================
+[strExeMD5Hash_1]4bf6fec2ba0d9e509213d8433f30d36b[strExeMD5Hash] // 1.1.1 (other)
