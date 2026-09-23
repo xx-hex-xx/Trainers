3a476ac064d8fe65bc9fb393011c8ab999f5299b
hex <7x.hex.x7@gmail.com>
2026-09-23 12:46:33 +0300

    Add ExeMD5Hashes.h with executable MD5 hashes

---
+++ Data/Public/ACBFR/1.0/Others/ExeMD5Hashes.h
@@ -0,0 +1,10 @@
+// |||||||||||||||||||||||||||||||||EXE HASHES|||||||||||||||||||||||||||||||||||
+// ====[ MAIN ]==================================================================
+const QString strExeMD5Hash_Steam = "2473b9eac35e39ac6fd787b1cdc02ddc"; // 1.0.7
+const QString strExeMD5Hash_Steam_106 = "eebd3fe9cb8ebc043283affbc033126a"; // 1.0.6
+const QString strExeMD5Hash_UbisoftConnect = "n/d"; // ?
+const QString strExeMD5Hash_UbisoftConnect_Plus = "n/d"; // ?
+const QString strExeMD5Hash_Other_1 = "3496bab89054c1b42c0b98e7f0a3b088"; // 1.0.6
+
+// ====[ ADDITIONALS ]===========================================================
+[strExeMD5Hash_1]3496bab89054c1b42c0b98e7f0a3b088[strExeMD5Hash] // 1.0.6 (other)
